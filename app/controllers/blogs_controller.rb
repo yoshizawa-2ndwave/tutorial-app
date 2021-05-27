1b498dbb883aa97283499c7d5ba2b4e01b275741
2ndwave.yoshizawa <yoshizawa@2ndwave.jp>
2021-05-27 17:40:02 +0900

    blogs new/create 登録処理

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -64,6 +64,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.fetch(:blog, {})
+      params.fetch(:blog, {}).permit(:title, :body)
     end
 end
