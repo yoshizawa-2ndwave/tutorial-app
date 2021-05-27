2130594dbf4d51dfdbb382eab95913ecc46c8aad
2ndwave.yoshizawa <yoshizawa@2ndwave.jp>
2021-05-27 13:23:55 +0900

    モデルの作成

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id              :bigint           not null, primary key
+#  body(本文)      :text(65535)      not null
+#  title(タイトル) :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class Blog < ApplicationRecord
 end
--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id              :bigint           not null, primary key
+#  body(本文)      :text(65535)      not null
+#  title(タイトル) :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'test_helper'
 
 class BlogTest < ActiveSupport::TestCase
